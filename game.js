4f72d7e06a7fcb86daf816e6a9497d0f435f140a
Daniil Burko <volatilepen690@gmail.com>
2023-03-10 11:39:26 +0100

    fix user input

--- game.js
+++ game.js
@@ -8,30 +8,31 @@ function pcChoice() {
 }
 
 function playerChoice() {
-  console.log("Welcome to Rock, Paper, Scissors game. Do you want to play? (y/n)");
-  let userInput = prompt();
-  while (userInput === "y") {
-    const pChoice = prompt("Choose rock, paper, or scissors:");
-    const cChoice = pcChoice();
-    console.log(`You chose ${pChoice}. The computer chose ${cChoice}.`);
-    if (pChoice === "rock" && cChoice === "paper") {
-      pcScore++;
-      console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
-    } else if (pChoice === "paper" && cChoice === "scissors") {
-      pcScore++;
-      console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
-    } else if (pChoice === "scissors" && cChoice === "rock") {
-      pcScore++;
-      console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
-    } else if (pChoice === cChoice) {
-      console.log(`It's a tie! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
-    } else {
-      playerScore++;
-      console.log(`You win! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+    console.log("Welcome to Rock, Paper, Scissors game. Do you want to play? (y/n)");
+    let userInput = prompt();
+    while (userInput === "y") {
+      const pChoice = prompt("Choose rock, paper, or scissors:");
+      const cChoice = pcChoice();
+      console.log(`You chose ${pChoice}. The computer chose ${cChoice}.`);
+      const lowercasePChoice = pChoice.toLowerCase();
+      if (lowercasePChoice === "rock" && cChoice === "paper") {
+        pcScore++;
+        console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+      } else if (lowercasePChoice === "paper" && cChoice === "scissors") {
+        pcScore++;
+        console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+      } else if (lowercasePChoice === "scissors" && cChoice === "rock") {
+        pcScore++;
+        console.log(`You lose! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+      } else if (lowercasePChoice === cChoice) {
+        console.log(`It's a tie! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+      } else {
+        playerScore++;
+        console.log(`You win! Your score is ${playerScore}. And computer score is: ${pcScore}.`);
+      }
+      userInput = prompt("Do you want to play again? (y/n)");
     }
-    userInput = prompt("Do you want to play again? (y/n)");
+    console.log("Thanks for playing!");
   }
-  console.log("Thanks for playing!");
-}
 
 playerChoice();
